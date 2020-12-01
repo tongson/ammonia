f34e343f6e4216da80f395c7eeec1aa9c27ce41f
Eduardo Tongson <propolice@gmail.com>
2020-12-01 19:12:35 +0800

    src: Unwinding during a panic across FFI boundaries may introduce undefined behavior. Hook panic() for a message to STDERR. panic=abort also reduces object size.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
+use std::panic;
 
 extern crate ammonia;
 
 #[no_mangle]
 pub extern "C" fn clean(h: *const c_char) -> *const c_char {
+    panic::set_hook(Box::new(move |_| eprintln!("panic: ammonia.clean()")));
     let html = unsafe { CStr::from_ptr(h).to_string_lossy().into_owned() };
     let c_str = CString::new(ammonia::clean(&html)).unwrap();
     let ptr = c_str.as_ptr();
@@ -14,6 +16,7 @@ pub extern "C" fn clean(h: *const c_char) -> *const c_char {
 
 #[no_mangle]
 pub extern "C" fn clean_text(h: *const c_char) -> *const c_char {
+    panic::set_hook(Box::new(move |_| eprintln!("panic: ammonia.clean_text()")));
     let html = unsafe { CStr::from_ptr(h).to_string_lossy().into_owned() };
     let c_str = CString::new(ammonia::clean_text(&html)).unwrap();
     let ptr = c_str.as_ptr();
